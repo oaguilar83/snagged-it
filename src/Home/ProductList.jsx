861f9e1b8f2942853d7caa0426bd40c41ca2b476
oaguilar83 <osc.aguilar83@gmail.com>
2025-10-14 16:19:04 -0700

    Fix list React key issue

--- src/Home/ProductList.jsx
+++ src/Home/ProductList.jsx
@@ -3,7 +3,7 @@ import styles from './ProductList.module.css'
 
 export default function ProductList({ products }) {
   const productList = products.map(product =>
-    <ProductCard product={product} />
+    <ProductCard product={product} key={product.id} />
   );
   return (
     <div className={styles.productList}>
