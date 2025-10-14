7736cabcb43cbca02010690a7c5a041840249b8f
oaguilar83 <osc.aguilar83@gmail.com>
2025-10-14 15:57:14 -0700

    Update ProductCard Title formatting
    Updated the ProductCard Title line limit to 2 and centered the title between the price and top of card.

--- src/Home/ProductCard.jsx
+++ src/Home/ProductCard.jsx
@@ -3,10 +3,10 @@ import styles from './ProductCard.module.css'
 export default function ProductCard({ product }) {
   return (
     <div className={styles.card}>
-      <figcaption>
+      <div className={styles.productTitleContainer}>
         <div className={styles.productTitle}>{product.title}</div>
-        <div className={styles.productPrice}>{product.price}</div>
-      </figcaption>
+      </div>
+      <div className={styles.productPrice}>{product.price}</div>
       <img height="150px" src={product.imagePath} alt={product.title} />
     </div>
   );
