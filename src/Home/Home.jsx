82561a31305aab76acd91906605e024b7ea994c9
oaguilar83 <osc.aguilar83@gmail.com>
2025-10-14 15:56:32 -0700

    Increase product title length for ellipsis testing

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,10 +2,10 @@ import ProductList from "./ProductList.jsx";
 import styles from './Home.module.css';
 
 const PRODUCTS = [
-  {id: 1, title: "Brand New Washington Nationals New Era Hat", price: "$15", imagePath: "./test_images/washington_nationals_hat.jpg"},
+  {id: 1, title: "Brand New Red Washington Nationals New Era Hat", price: "$15", imagePath: "./test_images/washington_nationals_hat.jpg"},
   {id: 2, title: "Used Late 2008 Apple Macbook Pro", price: "$450", imagePath: "./test_images/apple_macbook_pro.jpg"},
   {id: 3, title: "Used Chicago Bulls Jersey", price: "$75", imagePath: "./test_images/chicago_bulls_jersey.jpg"},
-  {id: 4, title: "Used Iphone 15 Pro", price: "$400", imagePath: "./test_images/iphone_15.jpg"}
+  {id: 4, title: "Used iPhone 15 Pro", price: "$400", imagePath: "./test_images/iphone_15.jpg"}
 ];
 
 export default function Home() {
