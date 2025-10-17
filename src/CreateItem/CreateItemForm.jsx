1af64b84efa6ffad1e0e7997e4c2accc48d49e67
oaguilar83 <osc.aguilar83@gmail.com>
2025-10-16 18:11:01 -0700

    Add requests from backend
    Add @tanstack/react-query and request to backend APIs to create and fetch items.

--- src/CreateItem/CreateItemForm.jsx
+++ src/CreateItem/CreateItemForm.jsx
@@ -1,25 +1,71 @@
 import styles from './CreateItemForm.module.css'
+import { useState } from 'react'
+import { useMutation } from '@tanstack/react-query';
 
 export default function CreateItemForm({ onCancel }) {
+  const [formData, setFormData] = useState({
+    title: '',
+    description: '',
+    price: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const submitForm = async (data) => {
+    const response = await fetch('/api/item', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    if (!response.ok) {
+      throw new Error('Form submission failed');
+    }
+    return response.json();
+  };
+
+  const mutation = useMutation( {
+    mutationFn: submitForm,
+    onSucess: (data) => {
+      console.log('Item created successfully:', data);
+    },
+    onError: (error) => {
+      console.error('Error creating item:', error);
+    }
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    mutation.mutate(formData);
+    onCancel();
+  }
+
   return (
     <div className={styles.formContainer}>
       <div className={styles.formHeader}>Create New Item</div>
-      <form className={styles.createItemForm}>
+      <form className={styles.createItemForm} onSubmit={handleSubmit}>
         <div className={styles.inputSection}>
           <div className={styles.titleTitle}>Title:</div>
-          <input type="text" placeholder="Title" />
+          <input type="text" name='title' value={formData.title} onChange={handleChange} placeholder="Title" />
         </div>
         <div className={styles.inputSection}>
           <div className={styles.descriptionTitle}>Description:</div>
-          <textarea rows="10" placeholder="Description"></textarea>
+          <textarea rows="10" name='description' value={formData.description} onChange={handleChange} placeholder="Description"></textarea>
         </div>
         <div className={styles.inputSection}>
           <div className={styles.priceTitle}>Price:</div>
-          <input type="text" placeholder="Price" />
+          <input type="text" name='price' value={formData.price} onChange={handleChange} placeholder="Price" />
         </div>
         <div className={styles.buttonContainer}>
-          <button className={styles.formButton} type="button" onClick={onCancel}>Cancel</button>
-          <button className={styles.formButton} type="submit">Submit</button>
+          <button type="button" onClick={onCancel}>Cancel</button>
+          <button type="submit" disabled={mutation.isPending}>Submit</button>
         </div>
       </form>
     </div>
--- src/Home/ItemList.jsx
+++ src/Home/ItemList.jsx
@@ -1,13 +1,28 @@
 import ItemCard from './ItemCard.jsx';
 import styles from './ItemList.module.css'
+import { useQuery } from '@tanstack/react-query';
 
-export default function ItemList({ items }) {
-  const itemList = items.map(item =>
-    <ItemCard item={item} key={item.id} />
-  );
+
+export default function ItemList() {
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['item'],
+    queryFn: async () => {
+      const response = await fetch('/api/item');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    }
+  })
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error: {error.message}</div>;
+
   return (
     <div id={styles.itemList}>
-      {itemList}
+      {data.map((item) => (
+        <ItemCard item={item} key={item.id} />
+      ))}
     </div>
   );
 }
